fbc93beda348a0b254bcc9c5523b5593f59b2605
Sachin Naranji <snaranji01@gmail.com>
2021-09-12 12:52:24 +0100

    added to event listener function to clear display div element when C key is clicked

--- calculator/index.js
+++ calculator/index.js
@@ -10,14 +10,21 @@ let numberPad2El = document.getElementById("keypad2");
 
 let keys = [
     ["x2", "sqrt", "C", "/"],
-    ["7", "8", "9","x"],
+    ["7", "8", "9", "x"],
     ["4", "5", "6", "-"],
     ["1", "2", "3", "+"],
     ["0", ".", "="]
 ]
 
-/* let keys_allrow = [...keys].flat();
-console.log(keys_allrow) */
+
+//define calculate from display function
+const calculate = screen_content => {
+    let input = screen_content.split("")
+}
+
+
+
+
 
 
 
@@ -25,7 +32,7 @@ console.log(keys_allrow) */
 keys.forEach((keyRow, index) => {
     let rowDiv = document.createElement("div");
     numberPadEl.appendChild(rowDiv);
-    rowDiv.id = "keyRow"+index.toString();
+    rowDiv.id = "keyRow" + index.toString();
     rowDiv.class = "keyRow"
 
     rowDiv.style.display = "flex";
@@ -56,10 +63,14 @@ keys.forEach((keyRow, index) => {
             let keyPressed = event.target.textContent;
             let numberKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
             if (numberKeys.includes(keyPressed)) {
+                console.log(`Pressed the ${keyPressed} key`)
                 screenEl.textContent += keyPressed;
+            } else if (keyPressed == "C") {
+                console.log("clear display")
+                screenEl.textContent = "";
             } 
-
-        })
+        }
+        )
     })
 })
 
@@ -74,7 +85,6 @@ let keysStylesToChange = [
 keysStylesToChange.forEach(keyToEdit => {
     keyToEdit.style.width = "70px"
 })
-    
 
 
 
@@ -88,13 +98,3 @@ keysStylesToChange.forEach(keyToEdit => {
 
 
 
-
-/* let numberPadEl = document.getElementById("keypad");
-[...new Array(10).keys()]
-    .forEach(element => {
-        let div = document.createElement("div");
-        numberPadEl.appendChild(div);
-        div.innerHTML += element;
-        div.class = "numberPadKey";
-    })
- */
