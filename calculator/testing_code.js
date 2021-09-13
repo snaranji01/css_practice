62cc5ec930c2425f8c826b8b139cb55bc45a9878
snaranji01 <snaranji01@gmail.com>
2021-09-13 02:25:43 +0100

    starting working on calc_function

--- calculator/testing_code.js
+++ calculator/testing_code.js
@@ -1,16 +1,54 @@
 let input1 = "3+3";
 let input2 = "3-3";
 
-allOperators = ["+", "-", "÷", "×"]
 
-const getOperator = (input, allOperators) => {
-    allOperators.forEach(el => {
-        input
-            .split("el") // split by test operator
-            .join("") // join again
-            .includes(el) // check if test operator was removed
-    })
-}
+
+/* const calcFunc = input => {
+    const getOperator = input => {
+        allOperators = ["+", "-", "÷", "×"]
+        for (let operator of allOperators) {
+            let splitInput = input.split(operator);
+            if (!splitInput.includes("+" || "-" || "×" || "÷")) {
+                console.log(`The operator detected is ${operator}.`)
+                return operator
+            }
+        }
+    }
+
+    let operation = getOperator(input);
+    let valInput = input.split(operation);
+    let output = 0;
+    switch (operation) {
+        case "+":
+            output = parseFloat(valInput[0]) + parseFloat(valInput[1])
+            break
+        case "-":
+            output = parseFloat(valInput[0]) - parseFloat(valInput[1])
+            break
+        case "×":
+            output = parseFloat(valInput[0]) * parseFloat(valInput[1])
+            break
+        case "÷":
+            output = parseFloat(valInput[0]) / parseFloat(valInput[1])
+            break
+        default:
+            output = "No recognised operator found"
+    }
+    console.log(output)
+} */
+
+
+
+/* const func = input => {
+    if (input.includes("+")) {
+        let vals = input1.replace("+", "")
+        console.log(vals)
+        console.log(vals.includes("+" | "-" | "×" | "÷"))
+    }
+} */
+
+
+calcFunc(input2)
 
 
 
