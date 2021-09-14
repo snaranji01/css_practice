239231d6c2d260405fc09aa326bde105e338f193
Sachin Naranji <snaranji01@gmail.com>
2021-09-14 17:09:28 +0100

    Made the 2 in x2 button superscript. Changed button definitions to their respective html codes instead of copy and pasting symbols into code

--- calculator/index.js
+++ calculator/index.js
@@ -8,33 +8,22 @@
 let numberPadEl = document.getElementById("keypad");
 let numberPad2El = document.getElementById("keypad2");
 
+let symbol_to_htmlcode = {
+    "√" : "&#8730",
+    "÷" : "&#247",
+    "×" : "&#215",
+    "−" : "&#8722",
+    "+" : "&#43",
+    "=" : "&#61"
+};
+
 let keys = [
-    ["x2", "√x", "C", "÷"],
-    ["7", "8", "9", "×"],
-    ["4", "5", "6", "−"],
-    ["1", "2", "3", "+"],
-    ["0", ".", "="]
+    ["x<sup>2</sup>", "<span>&#8730</span>x", "C", "<span>&#247</span>"],
+    ["7", "8", "9", "<span>&#215</span>"],
+    ["4", "5", "6", "<span>&#8722</span>"],
+    ["1", "2", "3", "<span>&#43</span>"],
+    ["0", ".", "<span>&#61</span>"]
 ]
-
-
-//define calculator key event listener function
-const keyEventListenerFunc = event => {
-    let screenEl = document.querySelector("#screen");
-    let keyPressed = event.target.textContent;
-    let numberKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "−", "×", "÷"];
-    if (numberKeys.includes(keyPressed)) {
-        console.log(`Pressed the ${keyPressed} key`)
-        screenEl.textContent += keyPressed;
-    } else if (keyPressed == "C") {
-        console.log("clear display")
-        screenEl.textContent = "";
-    } 
-}
-
-
-
-
-
 
 
 keys.forEach((keyRow, index) => {
@@ -70,17 +59,98 @@ keys.forEach((keyRow, index) => {
     })
 })
 
-
-
 // increase padding on 0, . and = keys
 let keysStylesToChange = [
     document.getElementById("0"),
     document.getElementById("."),
-    document.getElementById("=")
+    document.getElementById("<span>&#61</span>")
 ];
 keysStylesToChange.forEach(keyToEdit => {
     keyToEdit.style.width = "70px"
 })
+
+
+
+
+
+
+//define calculator key event listener function
+let saveKeyPresses = [];
+
+const keyEventListenerFunc = event => {
+    let screenEl = document.querySelector("#screen");
+    let keyPressed = event.target.textContent;
+    console.log(keyPressed)
+    saveKeyPresses.push(keyPressed);
+    console.log(saveKeyPresses)
+    
+    let numberKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "−", "×", "÷","."];
+    if (numberKeys.includes(keyPressed)) {
+        console.log(`Pressed the ${keyPressed} key`)
+        screenEl.textContent += keyPressed;
+    } else if (keyPressed === "C") {
+        console.log("clear display")
+        screenEl.textContent = "";
+    } else if (keyPressed === "=") {
+        screenEl.textContent = runCalculation(screenEl.textContent)
+    }
+}
+
+
+
+//runCalculation function from screen element input
+const runCalculation = input => {
+    //match operator
+    allOperators = ["+", "−", "÷", "×"];
+    let matchedOperator;
+    for (let operator of allOperators) {
+        if (input.includes(operator)) {
+            console.log(`The matched operator was ${operator}`);
+            matchedOperator = operator;
+        }
+    }
+    //perform calculation
+    let output;
+    let inputSplit = input.split(matchedOperator).map(value => parseFloat(value));
+    switch (matchedOperator) {
+        case "+":
+            output = inputSplit[0] + inputSplit[1];
+            console.log(output);
+            return output.toString();
+
+        case "−":
+            output = inputSplit[0] - inputSplit[1];
+            console.log(output);
+            return output.toString();
+
+        case "÷":
+            output = inputSplit[0] / inputSplit[1];
+            console.log(output);
+            return output.toString();
+
+        case "×":
+            output = inputSplit[0] * inputSplit[1];
+            console.log(output);
+            return output.toString();
+
+        default:
+            console.log("No matching conditions")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
